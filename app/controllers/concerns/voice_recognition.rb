e288794d11ffd67a2fa56092662671a28c19ee59
Diego Salazar <diego@greyrobot.com>
2017-10-24 22:24:07 -0400

    trying the straight json passed in as a keyfile

--- app/controllers/concerns/voice_recognition.rb
+++ app/controllers/concerns/voice_recognition.rb
@@ -4,6 +4,9 @@ module VoiceRecognition
   # GCP Creds is a json string that is base64 encoded
   # and stored in ENV.
   def voice_recog_service
-    @voice_recog_service ||= VoiceRecogService.new
+    @voice_recog_service ||= VoiceRecogService.new(
+      project: ENV.fetch('GCP_PROJECT', '1800Helpme'),
+      keyfile: ENV.fetch('GOOGLE_CLOUD_KEYFILE_JSON')
+    )
   end
 end
