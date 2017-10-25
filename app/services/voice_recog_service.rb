22614c9e6f35c275a6d8bbe9152abc280fa538e9
Diego Salazar <diego@greyrobot.com>
2017-10-24 22:43:21 -0400

    fix how keyfile was being passed into gcloud client

--- app/services/voice_recog_service.rb
+++ app/services/voice_recog_service.rb
@@ -4,18 +4,17 @@ require 'google/cloud/speech'
 # Speech to text from a Twilio call recording
 class VoiceRecogService
   include DefRetry
-  attr_reader :speech_decoder
 
-  def initialize(keyfile: '', project: '')
-    @speech_decoder ||= Google::Cloud::Speech.new(
-      project: project, keyfile: keyfile
-    )
+  def initialize(keyfile: '')
+    # Google::Cloud client needs a keyfile to authenticate,
+    # those are kept in ENV vars. More info: http://googlecloudplatform.github.io/google-cloud-ruby/#/docs/google-cloud/v0.44.0/guides/authentication
+    @speech_decoder = Google::Cloud::Speech.new(project: project)
   end
 
   def decode(call)
     retryable on: OpenURI::HTTPError do
       file = open call.recording_url
-      speech_decoder.
+      @speech_decoder.
         audio(file, language: 'en-US', sample_rate: 8000, encoding: :linear16).
         recognize.first
     end
