92199a404f01e17fb62f892afaca92b2622a00a3
Diego Salazar <diego@greyrobot.com>
2017-10-31 11:03:42 -0400

    cleanup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,6 @@ Rails.application.configure do
   # Send email in development mode?
   config.action_mailer.perform_deliveries = true
 
-
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
