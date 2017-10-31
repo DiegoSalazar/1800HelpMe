58c18c38b5751275c03e5c5bb42da0c84c43b1ce
Diego Salazar <diego@greyrobot.com>
2017-10-31 11:00:24 -0400

    update config

--- config/application.rb
+++ config/application.rb
@@ -4,21 +4,20 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require *Rails.groups
 Dotenv::Railtie.load
 
 module Www1800HelpMe
   class Application < Rails::Application
-
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
         view_specs: false,
         helper_specs: false,
         routing_specs: false,
-        controller_specs: false,
+        controller_specs: true,
         request_specs: false
-      g.fixture_replacement :factory_girl, dir: "spec/factories"
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
 
     # Initialize configuration defaults for originally generated Rails version.
