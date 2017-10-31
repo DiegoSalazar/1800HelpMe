e31376d1c21f4f298b16676d8116584bd75bb1c4
Diego Salazar <diego@greyrobot.com>
2017-10-31 02:17:02 -0400

    got it working again

--- app/models/concerns/contactable.rb
+++ app/models/concerns/contactable.rb
@@ -5,4 +5,12 @@ module Contactable
     has_many :phone_numbers, as: :contactable, dependent: :destroy
     has_many :addresses, as: :contactable, dependent: :destroy
   end
+
+  def phone
+    default_phone_number.number
+  end
+
+  def default_phone_number
+    phone_numbers.first
+  end
 end
--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -1,7 +1,10 @@
 module Searchable
+  extend ActiveSupport::Concern
   include PgSearch
-  pg_search_scope :search_by_full_name,
-    against: [:first_name, :middle_name, :last_name],
-    using: { tsearch: { any_word: true }, trigram: {}, dmetaphone: {}}
-  pg_search_scope :search_by_phone, against: [:phone]
+
+  included do
+    pg_search_scope :search_by_full_name,
+      against: [:first_name, :middle_name, :last_name],
+      using: { tsearch: { any_word: true }, trigram: {}, dmetaphone: {}}
+  end
 end
